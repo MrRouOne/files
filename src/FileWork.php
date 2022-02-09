d9f0fdef4208f33f259ebb083f56c0e68047c0c2
Кузюра Остап Дмитриевич <gr492_kod@ttit.local>
2022-02-09 11:47:30 +0700

    upload

--- src/FileWork.php
+++ src/FileWork.php
@@ -4,19 +4,49 @@ namespace FileWork;
 
 class FileWork
 {
-//    public function __construct(array $array = [])
-//    {
-//        $this->array = $array;
-//    }
-
-    public function getFile(string $filename): string
+    public function getFileName(string $filename): string
     {
         return $_FILES["$filename"]['name'];
     }
 
-    public function getUploadFolder(string $folder): string
+    public function getFileTempName(string $filename): string
     {
-        return $_FILES["$filename"]['name'];
+        return $_FILES["$filename"]['tmp_name'];
+    }
+
+    public function getFileType(string $filename): string
+    {
+        return $_FILES["$filename"]['type'];
+    }
+
+    public function getFileSize(string $filename): string
+    {
+        return $_FILES["$filename"]['size'];
+    }
+
+    public function getFileError(string $filename): string
+    {
+        return $_FILES["$filename"]['error'];
+    }
+
+    public function rootToUpload(string $filename, string $path, string $flag): string
+    {
+
+        if ($flag === 'this') {
+            return $path . $this->getFileName($filename);
+        }
+
+        if ($flag === "out") {
+            return "../" . $path . $this->getFileName($filename);
+        }
+        return "incorrect flag";
+    }
+
+    public function upload(string $filename, string $path, string $flag): void
+    {
+        echo($this->rootToUpload($filename, $path, $flag));
+
+        move_uploaded_file($this->getFileTempName("$filename"), $this->rootToUpload($filename, $path, $flag));
     }
 
 }
